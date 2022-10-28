7f4bf9f2e03bbb1e46905d2a1547720caaea3fc0
stavbers <reddevweb@gmail.com>
2022-10-29 02:41:02 +0300

    add slider quetes

--- src/Block.jsx
+++ src/Block.jsx
@@ -3,33 +3,24 @@ import {useState} from 'react'
 import { Button } from 'react-bootstrap';
 import './block.css'
 const Block = (props)=> {
-  const [text, setText] = useState('Lorem ipsum dolor sit amet consectetur adipisicing elit');
   const [click, setClick] = useState(0);
+  let activeSlide = 0;
   const nextBtn = ()=> {
- 
-   if(props.quotes.length > click && click <= props.quotes.length -1) {
-    setText(props.quotes[click])
-    setClick(click + 1)
-    console.log(click)
-   } else {
-    document.querySelector('.btn-next').classList.remove('btn-danger')
-    document.querySelector('.btn-next').classList.add('btn-secondary')
-   }
+    setClick(click + 1 < props.quotes.length ? click + 1 : 0)
    }
    const prevBtn = ()=> {
-     if(click > -1 ) {
-      setClick(click - 1)
-      console.log(click)
-      setText(props.quotes[click])
-     }
-
+    setClick(click - 1 < 0 ? props.quotes.length - 1:  click - 1)
    }
-
+   const quote = ()=> {
+    return (
+      <p>{props.quotes[click]}</p>
+    )
+  }
   return (
 <Fragment>
     <div className="wrp">
       <h2>{props.title}</h2>
-      <p className="wrp-info">{text}</p>
+      <p className="wrp-info">{quote()}</p>
     <div className="wrp-btn">
       <Button className={'btn-prev btn-secondary'} onClick={prevBtn}>Show</Button>
       <Button className={'btn-next btn btn-danger'} onClick={nextBtn}>{props.btn}</Button>
--- src/Model.js
+++ src/Model.js
@@ -3,9 +3,9 @@ const model = {
   btnTitle: 'Next',
   pt: 'tttttttaaaaaaa',
   quotes: [
-    'The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela',
-    'The way to get started is to quit talking and begin doing. -Walt Disney',
-    'Life is what happens when youre busy making other plans. -John Lennon'
+    '11The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela',
+    '22The way to get started is to quit talking and begin doing. -Walt Disney',
+    '33Life is what happens when youre busy making other plans. -John Lennon'
   ]
 }
 export default model
